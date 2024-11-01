e7b23808b471cf4354933682db2ca5dfc9d89959
TVT.IOT <tvt.iot@gmail.com>
2024-11-01 16:56:55 +0700

    sending emails using MailKit library

--- smtp-send-emails/README.md
+++ smtp-send-emails/README.md
@@ -198,6 +198,8 @@ $ ASPNETCORE_ENVIRONMENT=DEV dotnet run
 
 Environment is DEV
 Email Host is: smtp.gmail.com
+
+$ ASPNETCORE_ENVIRONMENT=QA dotnet run
 ```
 
 The following approach will load the content from the JSON file, then bind the content to two strongly-typed options/settings classes, which can be a lot cleaner than going value-by-value:
@@ -288,3 +290,112 @@ smtpClient.Send(message);
 
 # Sending email using MailKit
 
+MailKit is a powerful open-source .Net library that facilitates both the sending and receiving of emails. We use it in place of the SmtpClient class of the System.Net.Mail namespace. It’s highly recommended for those interested in taking a modern development approach as it contains modern protocols. For more info on MailKit see: https://github.com/jstedfast/MailKit.
+
+To use MailKit, first install it via NuGet. You can do this in the Package Manager Console of Visual Studio using the command:  
+```bash
+Install-Package MailKit
+dotnet add package MailKit
+```
+
+As soon as you install it, use the following C# MailKit code example to send your first email. Remember to customize it as per your needs. 
+```cs
+using System;
+using MailKit.Net.Smtp;
+using MimeKit;
+  
+namespace TestClient {
+  class Program
+  {
+    public static void Main (string[] args)
+    {
+      var email = new MimeMessage();
+
+      email.From.Add(new MailboxAddress("SenderName", "sender@email.com"));
+      email.To.Add(new MailboxAddress("RecipientName", "recipient@email.com"));
+
+      email.Subject = "Hello world";
+      email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { 
+        Text = "This is a test email sent using C#"
+      };
+
+      using (var smtp = new SmtpClient())
+      {
+        smtp.Connect("smtp.maileroo.com", 587, false);
+
+        // Note: only needed if the SMTP server requires authentication
+        smtp.Authenticate("smtp_username", "smtp_password");
+
+        smtp.Send(email);
+        smtp.Disconnect(true);
+      }
+    }
+  }
+}
+```
+
+More references about using MailKit:
+* https://medium.com/@abhinandkr56/how-to-send-emails-using-net-core-mailkit-and-googles-smtp-server-6521827c4198
+* https://www.smarterasp.net/support/kb/a2272/how-to-send-smtp-email-using-mailkit.aspx
+* https://ironpdf.com/blog/net-help/mailkit-csharp-guide/
+* https://mailtrap.io/blog/csharp-send-email/
+* https://jasonwatmore.com/post/2022/03/11/net-6-send-an-email-via-smtp-with-mailkit
+* https://medium.com/@gabrieletronchin/use-mailkit-and-greenmail-to-develop-a-email-interaction-0b0b27d2f85a
+* https://stackoverflow.com/questions/37853903/can-i-send-files-via-email-using-mailkit
+* http://www.mimekit.net/docs/html/Creating-Messages.htm
+
+
+# Send files via email using MailKit
+
+A simpler way to construct messages with attachments is to take advantage of the [BodyBuilder](http://www.mimekit.net/docs/html/T_MimeKit_BodyBuilder.htm) class.
+
+```cs
+var message = new MimeMessage ();
+message.From.Add (new MailboxAddress ("Joey", "joey@friends.com"));
+message.To.Add (new MailboxAddress ("Alice", "alice@wonderland.com"));
+message.Subject = "How you doin?";
+
+var builder = new BodyBuilder ();
+
+// Set the plain-text version of the message text
+builder.TextBody = @"Hey Alice,
+
+What are you up to this weekend? Monica is throwing one of her parties on
+Saturday. I was hoping you could make it.
+
+Will you be my +1?
+
+-- Joey
+";
+
+// We may also want to attach a calendar event for Monica's party...
+builder.Attachments.Add (@"C:\Users\Joey\Documents\party.ics");
+
+// Now we just need to set the message body and we're done
+message.Body = builder.ToMessageBody ();
+```
+
+You can also send multiple files using this approach directly. **Note: files used here is IEnumerable files **
+```cs
+try
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(emailService.FromFullName, emailService.FromEmail));
+            message.To.AddRange(emailsToSend.Select(x => new MailboxAddress(x)));
+            message.Subject = subject;
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = body;
+
+            foreach (var attachment in files)
+            {
+                if (attachment.Length > 0)
+                {
+                    string fileName = Path.GetFileName(attachment.FileName);
+                    builder.Attachments.Add(fileName, attachment.OpenReadStream());
+                }
+            }
+            message.Body = builder.ToMessageBody();
+           
+            }
+```
--- smtp-send-emails/SmtpTool/Program.cs
+++ smtp-send-emails/SmtpTool/Program.cs
@@ -2,11 +2,12 @@
 using System.Net;
 using System.Net.Mail;
 using Microsoft.Extensions.Configuration;
+using MimeKit;
 
 class Program
 {
     static void Main(string[] args)
-    {        
+    {
         var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
         Console.WriteLine($"Environment is {environment}");
 
@@ -22,44 +23,54 @@ class Program
 
         var smtpSettings = configuration.GetSection("Smtp").Get<SmtpSettings>();
         Console.WriteLine($"Smtp Username: {smtpSettings?.Username}");
-        
+
         var emailSettings = configuration.GetSection("Email").Get<EmailSettings>();
         Console.WriteLine($"Sending from Email: {emailSettings?.Sender}");
 
         // Test sending email via System.Net.Mail
-        var plainTextEmailRequest = new EmailRequest() {
-            FromEmail=emailSettings.Sender,
-            ToEmail=emailSettings.Recipient,
-            Subject="Hello world PlainText", 
-            Body="This is a test email sent using C#.NET",
-            IsHtmlBody = false             
+        var plainTextEmailRequest = new EmailRequest()
+        {
+            FromEmail = emailSettings.Sender,
+            ToEmail = emailSettings.Recipient,
+            Subject = "Hello world PlainText",
+            Body = "This is a test email sent using C#.NET",
+            IsHtmlBody = false
         };
         //SendEmailViaNetMail(plainTextEmailRequest, smtpSettings);
+        //SendEmailViaMailKit(plainTextEmailRequest, smtpSettings);
 
-        var htmlEmailRequest = new EmailRequest() {
-            FromEmail=emailSettings.Sender,
-            ToEmail=emailSettings.Recipient,
-            Subject="Hello world HtmlFormat", 
-            Body="<em>It's great to use HTML in mail!!</em>",
-            IsHtmlBody = true             
+        var htmlEmailRequest = new EmailRequest()
+        {
+            FromEmail = emailSettings.Sender,
+            ToEmail = emailSettings.Recipient,
+            Subject = "Hello world HtmlFormat",
+            Body = "<em>It's great to use HTML in mail!!</em>",
+            IsHtmlBody = true
         };
 
-        SendEmailViaNetMail(htmlEmailRequest, smtpSettings);
+        //SendEmailViaNetMail(htmlEmailRequest, smtpSettings);
+        //SendEmailViaMailKit(htmlEmailRequest, smtpSettings);
 
-        var attachmentEmailRequest = new EmailRequest() {
-            FromEmail=emailSettings.Sender,
-            ToEmail=emailSettings.Recipient,
-            Subject="Hello world Attachment", 
-            Body="This is a test email sent using C#.NET",
-            IsHtmlBody=false,
-            Attachments = new List<string> {"instruction.txt", "8-ball_rules_bca.pdf"}
+        var attachmentEmailRequest = new EmailRequest()
+        {
+            FromEmail = emailSettings.Sender,
+            ToEmail = emailSettings.Recipient,
+            Subject = "Hello world Attachment",
+            Body = "<em>It's great to use HTML in mail!!</em>",
+            IsHtmlBody = true,
+            Attachments = new List<string> { "instruction.txt", "8-ball_rules_bca.pdf" }
         };
 
         //SendEmailViaNetMail(attachmentEmailRequest, smtpSettings);
+        SendEmailViaMailKit(attachmentEmailRequest, smtpSettings);
+
+
+        
     }
 
-    public class SmtpSettings {
-        public string Host { get; set; }   
+    public class SmtpSettings
+    {
+        public string Host { get; set; }
         public int Port { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
@@ -67,24 +78,27 @@ class Program
         public string From { get; set; }
     }
 
-    public class EmailSettings {
-        public string Sender {get; set;}
-        public string Recipient {get; set;}        
+    public class EmailSettings
+    {
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
     }
 
-    public class EmailRequest {
-        public string Subject {get; set;}
-        public string Body {get; set;}
-        public bool IsHtmlBody {get; set;}
-        public string FromEmail {get; set;}
-        public string ToEmail {get; set;}
-        public List<string> Attachments {get; set;}
+    public class EmailRequest
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsHtmlBody { get; set; }
+        public string FromEmail { get; set; }
+        public string ToEmail { get; set; }
+        public List<string> Attachments { get; set; }
     }
 
-    public static void SendEmailViaNetMail(EmailRequest emailRequest, SmtpSettings smtpSettings) {        
+    public static void SendEmailViaNetMail(EmailRequest emailRequest, SmtpSettings smtpSettings)
+    {
         var senderEmail = emailRequest.FromEmail;
         var recipientEmail = emailRequest.ToEmail;
-        
+
         MailMessage mailMessage = new MailMessage();
         mailMessage.From = new MailAddress(senderEmail);
         mailMessage.To.Add(recipientEmail);
@@ -92,18 +106,20 @@ class Program
 
         mailMessage.Body = emailRequest.Body;
         mailMessage.IsBodyHtml = emailRequest.IsHtmlBody;
-        
-        if (emailRequest.Attachments != null && emailRequest.Attachments.Count>0) {
-            foreach (var filePath in emailRequest.Attachments) {
+
+        if (emailRequest.Attachments != null && emailRequest.Attachments.Count > 0)
+        {
+            foreach (var filePath in emailRequest.Attachments)
+            {
                 // Create a new Attachment object
                 Attachment attachment = new Attachment(filePath);
 
                 // Add the attachment to the MailMessage object
                 mailMessage.Attachments.Add(attachment);
-            }           
+            }
         }
 
-        SmtpClient smtpClient= new SmtpClient();
+        SmtpClient smtpClient = new SmtpClient();
         smtpClient.Host = smtpSettings.Host;
         smtpClient.Port = smtpSettings.Port;
         smtpClient.UseDefaultCredentials = false;
@@ -119,5 +135,78 @@ class Program
         {
             Console.WriteLine("Error: " + ex.Message);
         }
+    }
+
+    public static void SendEmailViaMailKit(EmailRequest emailRequest, SmtpSettings smtpSettings)
+    {
+        try
+        {
+            var mailMessage = new MimeMessage();
+
+            mailMessage.From.Add(new MailboxAddress("SenderName", emailRequest.FromEmail));
+            mailMessage.To.Add(new MailboxAddress("RecipientName", emailRequest.ToEmail));
+
+            mailMessage.Subject = emailRequest.Subject;
+            
+            /*
+            if (emailRequest.IsHtmlBody)
+            {
+                mailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                {
+                    Text = emailRequest.Body
+                };
+            }
+            else
+            {
+                mailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Plain)
+                {
+                    Text = emailRequest.Body
+                };
+            }
+            */
+
+            var bodyBuilder = new BodyBuilder();
+            if (emailRequest.IsHtmlBody) {
+                // Set the html version of the message text
+                bodyBuilder.HtmlBody = emailRequest.Body;
+            } else {
+                // Set the plain-text version of the message text
+                bodyBuilder.TextBody = emailRequest.Body;
+            }
+
+            if (emailRequest.Attachments != null && emailRequest.Attachments.Count > 0)
+            {
+                foreach (var filePath in emailRequest.Attachments)
+                {
+                    bodyBuilder.Attachments.Add(filePath);
+                }
+            }
+
+            // Now we just need to set the message body
+            // Ref: http://www.mimekit.net/docs/html/Creating-Messages.htm
+            mailMessage.Body = bodyBuilder.ToMessageBody();
+
+            
+            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {                
+                if (smtpSettings.EnableSsl) {
+                    smtp.Connect(smtpSettings.Host, smtpSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+                } else {
+                    smtp.Connect(smtpSettings.Host, smtpSettings.Port, MailKit.Security.SecureSocketOptions.Auto);
+                }                
+                
+                // Note: only needed if the SMTP server requires authentication
+                smtp.Authenticate(smtpSettings.Username, smtpSettings.Password);
+
+                smtp.Send(mailMessage);
+                smtp.Disconnect(true);
+            }
+            Console.WriteLine("Email Sent Successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
     }
 }
